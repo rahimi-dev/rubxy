792517eb36c40edacd1d0242a5e472c64d70cc51
Amirali Rahimi <contruzog3704@gmail.com>
2025-10-10 18:56:51 +0330

    Update library

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="rubxy",
     author="Amirali Rahimi",
     author_email="amiralirahimi769@gmail.com",
-    version="0.1.2b",
+    version="0.1.3b",
     description="A Python library for interacting with the Rubika bot API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
